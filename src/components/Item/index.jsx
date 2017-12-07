cb45a3e8db1e8e67044734150b19ba356b29e935
Andrei Neacșu <amneacsu@gmail.com>
2017-12-08 00:22:52 +0200

    remove img

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 
-import Image from 'components/Image';
-import Video from 'components/Video';
 import delayFirstRender from 'util/delayFirstRender';
+
+import css from './style.css';
 
 class Item extends Component {
   componentWillUnmount() {
@@ -20,26 +20,20 @@ class Item extends Component {
 
     const url = this.props.item.url;
 
-    const match = url.match(/\.(\w*)$/);
-    const ext = match ? match[1] : null;
-
-    switch (ext) {
-      case 'gifv':
-      case 'gif':
-      case 'jpg':
-      case 'jpeg':
-      case 'png':
-        return <Image url={url} onClick={onClick} setRef={(e) => this.setRef(e)} />;
-
-      case 'webm':
-      case 'mp4':
-        return <Video url={url} onClick={onClick} setRef={(e) => this.setRef(e)} />;
-
-      default:
-        console.warn(`Unhandled file extension "${ext}" for file "${url}"`);
-        return null;
-    }
-
+    return (
+      <video
+        className={css.Video}
+        ref={(e) => this.setRef(e)}
+        autoPlay
+        controls
+        onClick={onClick}
+        onEnded={() => {
+          onClick();
+        }}
+      >
+        <source src={url} />
+      </video>
+    );
   }
 }
 
